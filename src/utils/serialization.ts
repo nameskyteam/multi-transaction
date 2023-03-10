76d1adb398803b99513977c8979f5057d731c78b
sakura <zjchen1234@foxmail.com>
2023-03-11 00:47:27 +0800

    chore

--- src/utils/serialization.ts
+++ src/utils/serialization.ts
@@ -1,10 +1,10 @@
 // Json serialize, if data is bytes, skip.
 export function bytesOrJsonStringify<T>(data: T): Uint8Array {
   const isUint8Array = (data as Uint8Array).byteLength && (data as Uint8Array).byteLength === (data as Uint8Array).length;
-  return isUint8Array ? (data as Uint8Array) : new Uint8Array(Buffer.from(JSON.stringify(data)));
+  return isUint8Array ? new Uint8Array(data as Uint8Array) : new Uint8Array(Buffer.from(JSON.stringify(data)));
 }
 
 // Json deserialize
 export function jsonParse<T>(rawData: Uint8Array): T {
-  return JSON.parse(rawData.toString())
+  return JSON.parse(Buffer.from(rawData).toString())
 }
