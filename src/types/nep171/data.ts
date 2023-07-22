c505a959c5c7fe0969ca0551f4c44ee66621a08b
sakura <zjchen1234@foxmail.com>
2023-07-22 23:09:49 +0800

    chore: comments

--- src/types/nep171/data.ts
+++ src/types/nep171/data.ts
@@ -6,26 +6,26 @@ export interface Token {
 }
 
 export interface TokenMetadata {
-  title?: string; // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
-  description?: string; // free-form description
-  media?: string; // URL to associated media, preferably to decentralized, content-addressed storage
-  media_hash?: string; // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
-  copies?: number; // number of copies of this set of metadata in existence when token was minted.
-  issued_at?: string; // ISO 8601 datetime when token was issued or minted
-  expires_at?: string; // ISO 8601 datetime when token expires
-  starts_at?: string; // ISO 8601 datetime when token starts being valid
-  updated_at?: string; // ISO 8601 datetime when token was last updated
-  extra?: string; // anything extra the NFT wants to store on-chain. Can be stringified JSON.
-  reference?: string; // URL to an off-chain JSON file with more info.
-  reference_hash?: string; // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+  title?: string;
+  description?: string;
+  media?: string;
+  media_hash?: string;
+  copies?: number;
+  issued_at?: string;
+  expires_at?: string;
+  starts_at?: string;
+  updated_at?: string;
+  extra?: string;
+  reference?: string;
+  reference_hash?: string;
 }
 
 export interface NFTContractMetadata {
-  spec: string; // required, essentially a version like "nft-1.0.0"
-  name: string; // required, ex. "Mosaics"
-  symbol: string; // required, ex. "MOSIAC"
-  icon?: string; // Data URL
-  base_uri?: string; // Centralized gateway known to have reliable access to decentralized storage assets referenced by `reference` or `media` URLs
-  reference?: string; // URL to a JSON file with more info
-  reference_hash?: string; // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+  spec: string;
+  name: string;
+  symbol: string;
+  icon?: string;
+  base_uri?: string;
+  reference?: string;
+  reference_hash?: string;
 }
