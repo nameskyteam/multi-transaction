099eab6156a9a57a4e7937e98cefb3f863655d9f
sakura <zjchen1234@foxmail.com>
2023-07-25 22:53:51 +0800

    refactor

--- src/utils/Amount.ts
+++ src/utils/Amount.ts
@@ -9,19 +9,30 @@ export class Amount {
   private constructor() {}
 
   /**
-   * Parse from specific units.
+   * Parse from specific units and return a fixed string.
    * @example
    * const rawAmount = Amount.parse('5', 'near'); // '5000000000000000000000000'
    * @param amount Human readable amount
    * @param units Units decimals
    */
   static parse(amount: BigNumberish, units: 'near' | number): string {
-    units = units === 'near' ? Amount.NEAR_DECIMALS : units;
-    return BigNumber(amount).shiftedBy(units).toFixed(0);
+    return Amount.parseBigNumber(amount, units).toFixed();
   }
 
   /**
-   * Format in specific units.
+   * Parse from specific units and return a BigNumber.
+   * @example
+   * const rawAmount = Amount.parseBigNumber('5', 'near'); // BigNumber('5000000000000000000000000')
+   * @param amount Human readable amount
+   * @param units Units decimals
+   */
+  static parseBigNumber(amount: BigNumberish, units: 'near' | number): BigNumber {
+    units = units === 'near' ? Amount.NEAR_DECIMALS : units;
+    return BigNumber(amount).shiftedBy(units).decimalPlaces(0);
+  }
+
+  /**
+   * Format in specific units and return a fixed string.
    * @example
    * const humanReadableAmount = Amount.format('5000000000000000000000000', 'near'); // '5'
    * @param amount Raw amount
@@ -29,12 +40,23 @@ export class Amount {
    * @param decimalPlaces Decimal places
    */
   static format(amount: BigNumberish, units: 'near' | number, decimalPlaces?: number): string {
-    units = units === 'near' ? Amount.NEAR_DECIMALS : units;
-    amount = BigNumber(amount).shiftedBy(-units);
+    amount = Amount.formatBigNumber(amount, units);
     if (decimalPlaces) {
       return amount.toFixed(decimalPlaces);
     } else {
       return amount.toFixed();
     }
+  }
+
+  /**
+   * Format in specific units and return a BigNumber.
+   * @example
+   * const humanReadableAmount = Amount.formatBigNumber('5000000000000000000000000', 'near'); // BigNumber('5')
+   * @param amount Raw amount
+   * @param units Units decimals
+   */
+  static formatBigNumber(amount: BigNumberish, units: 'near' | number): BigNumber {
+    units = units === 'near' ? Amount.NEAR_DECIMALS : units;
+    return BigNumber(amount).shiftedBy(-units);
   }
 }
--- src/utils/Gas.ts
+++ src/utils/Gas.ts
@@ -5,24 +5,35 @@ export class Gas {
   static TERA_DECIMALS = 12 as const;
   static GIGA_DECIMALS = 9 as const;
 
-  // 30 Tera
+  // 30 Tera gas
   static DEFAULT = '30000000000000' as const;
 
   private constructor() {}
 
   /**
-   * Parse from specific units.
+   * Parse from specific units and return a fixed string.
    * @example
    * const rawGas = Gas.parse('5', 'tera'); // '5000000000000'
    * @param gas Human readable gas
    * @param units Units decimals
    */
   static parse(gas: BigNumberish, units: 'tera' | 'giga' | number): string {
+    return Gas.parseBigNumber(gas, units).toFixed();
+  }
+
+  /**
+   * Parse from specific units and return a BigNumber.
+   * @example
+   * const rawGas = Gas.parseBigNumber('5', 'tera'); // BigNumber('5000000000000')
+   * @param gas Human readable gas
+   * @param units Units decimals
+   */
+  static parseBigNumber(gas: BigNumberish, units: 'tera' | 'giga' | number): BigNumber {
     if (units === 'tera') {
       units = Gas.TERA_DECIMALS;
     } else if (units === 'giga') {
       units = Gas.GIGA_DECIMALS;
     }
-    return BigNumber(gas).shiftedBy(units).toFixed(0);
+    return BigNumber(gas).shiftedBy(units).decimalPlaces(0);
   }
 }
