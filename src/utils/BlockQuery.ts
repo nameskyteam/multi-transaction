6b8c64793dfc23fb7b75ac9b0f3aa3858b90e541
sakura <zjchen1234@foxmail.com>
2024-03-19 13:01:57 +0800

    feat: convert BlockQuery

--- src/utils/BlockQuery.ts
+++ src/utils/BlockQuery.ts
@@ -1,4 +1,5 @@
 import { BlockReference } from 'near-api-js/lib/providers/provider';
+import { Provider } from '@near-wallet-selector/core/src/lib/services';
 
 export class BlockQuery {
   private readonly reference: BlockReference;
@@ -62,5 +63,95 @@ export class BlockQuery {
    */
   static hash(h: string): BlockQuery {
     return BlockQuery.from({ blockId: h });
+  }
+
+  /**
+   * Convert current block query to that with certain block height.
+   * This is useful when need multiple queries at the same block
+   *
+   * @example
+   * const blockQuery = BlockQuery.final;
+   *
+   * const supply1 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   *
+   * // supply2 may not be equal to supply1 because they may be queried at different block
+   * const supply2 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   *
+   *@example
+   * const blockQuery = await BlockQuery.final.height(provider);
+   *
+   * const supply1 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   *
+   * // supply2 must be equal to supply1 because they are queried at the same block
+   * const supply2 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   */
+  async height(provider: Provider): Promise<BlockQuery> {
+    if ('blockId' in this.reference && typeof this.reference.blockId === 'number') {
+      return BlockQuery.height(this.reference.blockId);
+    }
+
+    const block = await provider.block(this.reference);
+    return BlockQuery.height(block.header.height);
+  }
+
+  /**
+   * Convert current block query to that with certain block hash.
+   * This is useful when need multiple queries at the same block
+   *
+   * @example
+   * const blockQuery = BlockQuery.final;
+   *
+   * const supply1 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   *
+   * // supply2 may not be equal to supply1 because they may be queried at different block
+   * const supply2 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   *
+   *@example
+   * const blockQuery = await BlockQuery.final.hash(provider);
+   *
+   * const supply1 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   *
+   * // supply2 must be equal to supply1 because they are queried at the same block
+   * const supply2 = await account.view({
+   *   contractId: 'wrap.near',
+   *   methodName: 'ft_total_supply',
+   *   blockQuery
+   * });
+   */
+  async hash(provider: Provider): Promise<BlockQuery> {
+    if ('blockId' in this.reference && typeof this.reference.blockId === 'string') {
+      return BlockQuery.hash(this.reference.blockId);
+    }
+
+    const block = await provider.block(this.reference);
+    return BlockQuery.hash(block.header.hash);
   }
 }
